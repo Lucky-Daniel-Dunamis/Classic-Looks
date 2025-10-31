ff1f86fb40ecd2e190d29f50b12b7d50a8c337b6
Lucky Daniel Dunamis Chukwuemeka <luckudanielr@gmail.com>
2025-10-31 14:54:34 +0100

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    id="about"
+    class="min-h-screen scroll-mt-20 bg-gradient-to-b from-gray-50 to-white py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="max-w-7xl mx-auto">
+      <div class="flex flex-col md:flex-row lg:flex-row gap-12 mb-20">
+        <!-- Image Section -->
+        <div class="flex-[1] sm:flex-[0.5] md:flex-[0.5] lg:flex-[0.5]">
+          <div
+            class="relative rounded-2xl overflow-hidden h-96 shadow-xl w-auto"
+          >
+            <img
+              src="../assets/About.jpg"
+              alt="Luxury perfume"
+              class="w-full h-full"
+            />
+            <div
+              class="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50"
+            ></div>
+            <div class="absolute bottom-0 left-0 p-6 text-white">
+              <h3 class="text-2xl font-bold">Since 2015</h3>
+              <p class="mt-2">We don’t just sell luxury, we live it</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Text Content -->
+        <div class="flex-[1] sm:flex-[1] md:flex-[1] lg:flex-1 flex-col justify-center">
+          <h2 class="text-4xl font-bold text-amber-900 mb-6 ">About Us</h2>
+          <p class="text-lg text-gray-600 mb-4">
+At Classic Looks, we believe true style is never loud; it's refined. Founded in 2015, our journey began with a simple vision: to curate the world's finest suits and luxury perfumes for those who understand that elegance is a language spoken without words.
+Every stitch in our bespoke suits tells a story of Italian craftsmanship from Biella's finest fabrics. Every fragrance is a signature, hand-selected from legendary perfumers in Grasse, Paris, and beyond. We don't follow trends. We define them.
+
+          </p>
+          <div class="grid grid-cols-2 gap-4 mt-4">
+            <div
+              class="bg-white p-4 rounded-lg shadow-sm border border-gray-100"
+            >
+              <h4 class="font-semibold text-pink-600">500+</h4>
+              <p class="text-gray-600">Unique fragrances</p>
+            </div>
+            <div
+              class="bg-white p-4 rounded-lg shadow-sm border border-gray-100"
+            >
+              <h4 class="font-semibold text-pink-600">50+</h4>
+              <p class="text-gray-600">Countries worldwide</p>
+            </div>
+            <div
+              class="bg-white p-4 rounded-lg shadow-sm border border-gray-100"
+            >
+              <h4 class="font-semibold text-pink-600">100+</h4>
+              <p class="text-gray-600">Customer satifaction</p>
+            </div>
+            <div
+              class="bg-white p-4 rounded-lg shadow-sm border border-gray-100"
+            >
+              <h4 class="font-semibold text-pink-600">24/7</h4>
+              <p class="text-gray-600">Customer support</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script></script>
---
+++ src/components/Categories.vue
@@ -0,0 +1,90 @@
+<template>
+  <section id="categories" class="py-16 scroll-mt-20 bg-white">
+    <div class="container mx-auto px-4">
+      <!-- Section Header -->
+      <div class="text-center mb-12">
+        <h2 class="text-3xl font-bold text-amber-950 mb-3">
+          Explore Our Fragrance Collections
+        </h2>
+        <p class="text-lg text-gray-800 max-w-2xl mx-auto">
+          Discover scents that match your personality
+        </p>
+      </div>
+
+      <!-- Categories Grid -->
+      <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
+        <div
+          v-for="(category, index) in categories"
+          :key="index"
+          class="relative group overflow-hidden rounded-lg transition-shadow duration-300"
+        >
+          <div class="h-64" overflow-hidden>
+            <img
+              :src="category.image"
+              :alt="category.alt"
+              class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
+            />
+          </div>
+          <div
+            class="absolute inset-0 bg-gradient-to from-black/60 via-transparent to-transparent"
+          >
+            <div class="absolute bottom-0 left-0 p-6">
+              <h3 class="text-xl font-semibold text-amber-950">
+                {{ category.title }}
+              </h3>
+              <p class="text-amber-950 mt-1">{{ category.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import { ref } from "vue";
+import Per1 from "../assets/Per1.png";
+import Per2 from "../assets/Per2.png";
+import Per3 from "../assets/Per3.png";
+import Per4 from "../assets/Per4.png";
+import Per5 from "../assets/Per5.png";
+import Per6 from "../assets/Per6.png";
+
+const categories = ref([
+  {
+    image: Per1,
+    title: "Men's Collection",
+    description: "Imperial Majesty",
+    alt: "Men's Collection",
+  },
+  {
+    image: Per2,
+    title: "Women's Collection",
+    description: "Amouage Tribute Attar",
+    alt: "Women's Collection",
+  },
+  {
+    image: Per3,
+    title: "Men's Collection",
+    description: "Tom Ford",
+    alt: "Men's Collection",
+  },
+  {
+    image: Per4,
+    title: "Men's Collection",
+    description: "Royal Oud",
+    alt: "Men's Collection",
+  },
+  {
+    image: Per5,
+    title: "Men's Collection",
+    description: "The Moon",
+    alt: "Men's Collection",
+  },
+  {
+    image: Per6,
+    title: "Women's Collection",
+    description: "Flowerbomb",
+    alt: "Men's Collection",
+  },
+]);
+</script>
